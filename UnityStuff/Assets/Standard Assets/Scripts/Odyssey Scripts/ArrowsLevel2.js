4b5f273dce78ef576625331750384ac34d86838e
dpumford <dpumford@hotmail.com>
2013-12-12 16:01:07 -0600

    Added Javelin, starting GUI for Level 2, created ArrowScript

---
+++ UnityStuff/Assets/Standard Assets/Scripts/Odyssey Scripts/ArrowsLevel2.js
@@ -0,0 +1,40 @@
+﻿// Shoot arrows:
+// If player presses LMB, make tofu ball appear, then move ball by force of 300 units
+// 
+
+var arrow : Rigidbody;
+private var numArrows : int;
+var force : float = 100;
+var lowArrows = false;
+
+function Start () {
+	numArrows = GuiLevel2.Instance.numArrows;
+}
+
+function Update () {
+	if( Input.GetButtonDown("Fire1"))
+	{
+		if(numArrows > 0) {
+			var clone : Rigidbody = Instantiate(arrow, transform.position, transform.rotation);	// this makes arrow appear
+			
+			Physics.IgnoreCollision(clone.collider, transform.root.collider);
+			
+			clone.AddForce( force * transform.forward);	// make ball move. Apply force in the forward direction by amount "force"
+			
+			numArrows--;
+			GuiLevel2.Instance.numArrows--;
+		}
+		
+		if(numArrows < 6) {
+		
+			if(!lowArrows) {
+				GuiLevel2.Instance.SetMessage("I'm running low on arrows!");
+			}
+			
+			lowArrows = true;
+		}
+	
+	
+	}
+
+}
---
+++ UnityStuff/Assets/Standard Assets/Scripts/Odyssey Scripts/GuiLevel2.js
@@ -0,0 +1,45 @@
+﻿private var message = "";
+private var messageTimer = 0.0;
+var messageDisplayTime = 10.0;
+
+var numArrows : int;
+static var Instance : GuiLevel2;
+
+function Awake () {
+	Instance = this;
+}
+
+function Start () {
+	
+}
+
+function Update () {
+	if (messageTimer > 0) {
+		messageTimer -= Time.deltaTime;
+	}
+	else {
+		message = "";
+	}
+}
+
+function OnGUI() {
+//	var arrowScript : ArrowsLevel2;
+	
+//	var caches = GameObject.FindGameObjectsWithTag("cache");
+//	var numCachedWeapons = 0;
+//	
+//	for (var c = 0; c < caches.Length; c++) {
+//		var cacheScript : CacheScript = caches[c].GetComponent(CacheScript);
+//	
+//		numCachedWeapons += cacheScript.NumWeapons();
+//	}
+
+	GUI.Box(Rect(0, 0, 100, 32), numArrows + "");
+//	GUI.Box(Rect(100, 0, 100, 32), numCachedWeapons + "");
+	GUI.Box(Rect(100, 0, 500, 32), message);
+}
+
+function SetMessage(msg : String) {
+	message = msg;
+	messageTimer = messageDisplayTime;
+}
